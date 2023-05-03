5a56a2cf15486638025cde85c3c45c1aba2816b0
Rohit Jadhav <rohitjadhav@nimapinfotech.com>
2023-05-03 18:11:24 +0530

    first commit

---
+++ src/main/java/com/example/emailverification/security/UserRegistrationDetailsService.java
@@ -0,0 +1,25 @@
+package com.example.emailverification.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.emailverification.user.UserRepository;
+
+/**
+ * @author Sampson Alfred
+ */
+@Service
+@RequiredArgsConstructor
+public class UserRegistrationDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(UserRegistrationDetails::new)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+    }
+}
